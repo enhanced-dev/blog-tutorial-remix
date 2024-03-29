6f99d975ce97a651b5e9e68eea57ac29c196b30d
Reuben Ellis <ethriel3695@gmail.com>
2024-03-29 12:27:17 -0600

    add storybook global

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,6 +6,22 @@ import { withThemeByDataAttribute } from "@storybook/addon-styling";
 import "../app/tailwind.css";
 
 const preview: Preview = {
+  globalTypes: {
+    locale: {
+      description: "Internationalization locale",
+      defaultValue: "en",
+      toolbar: {
+        icon: "globe",
+        items: [
+          { value: "en", right: "🇺🇸", title: "English" },
+          { value: "fr", right: "🇫🇷", title: "Français" },
+          { value: "es", right: "🇪🇸", title: "Español" },
+          { value: "zh", right: "🇨🇳", title: "中文" },
+          { value: "kr", right: "🇰🇷", title: "한국어" },
+        ],
+      },
+    },
+  },
   parameters: {
     actions: { argTypesRegex: "^on.*" },
     controls: {
